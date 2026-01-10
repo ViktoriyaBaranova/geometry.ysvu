b8e70395925d46227d47d27c7d67d023ee05ed73
ViktoriyaBaranova <87519038+ViktoriyaBaranova@users.noreply.github.com>
2026-01-10 18:53:00 +1000

    Add files via upload

---
+++ lesson-6-vector-projection/task-6-1-match/script_6_1.js
@@ -0,0 +1,160 @@
+//https://codepen.io/Coding_Journey/pen/LYPNmpe
+//https://www.youtube.com/watch?v=7HUCAYMylCQ
+"use strict";
+
+const sectionDraggable = document.querySelector(".draggable-items");
+const sectionMatching = document.querySelector(".matching-elements");
+const condition = document.querySelector(".condition");
+const btnCheck = document.querySelector(".check-btn");
+const btnRestart = document.querySelector(".restart-btn");
+const spanResult = document.querySelector(".span-result");
+const score = 10;
+let count, mark, cardData, fieldData;
+
+
+const randomize = (arr) =>{
+    return [...arr].map(a => ({ value: a, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(a => a.value);   
+};
+
+
+const matchingGenerator = (cardData = randomize(data).slice(0, totalDraggableItems))=>{
+    const uniqueName = new Set(), uniqueText = new Set();
+    
+    condition.innerHTML = `<div class="hint"><h3>${cond.header}</h3><img class="sistem-hint" src="./../../paint/hint.png"/><img class="teory-hint" src="./../../paint/lamp.png"/></div><p>${cond.condition}</p>`;
+ 
+    cardData.forEach(el=>{
+        sectionDraggable.innerHTML += el.innerHtml;
+    });
+    [...sectionDraggable.children].forEach((card, ind)=>{
+        card.classList.add('draggable');
+        card.setAttribute("draggable", "true");
+        card.setAttribute("id", cardData[ind].name);
+    });
+    //remember objects with the same value
+    fieldData = randomize(cardData);
+    fieldData.forEach((item)=>{
+        uniqueName.add(item.name);
+        uniqueText.add(item.text);
+    });
+    //create fields for insertion
+    [...uniqueName].forEach((item, ind)=>{
+        const element = document.createElement("div");
+        element.classList.add('matching-element');
+        element.innerHTML = `<span class="label">${[...uniqueText][ind]}</span><span class="droppable" drop-id="${item}"></span>`;
+        sectionMatching.appendChild(element);
+    });
+    cond.fieldData = fieldData;
+    cond.cardData = cardData;
+    console.log(fieldData);
+}
+
+const commitMatching = ()=>{
+    const draggableItems = document.querySelectorAll(".draggable");
+    const droppableItems = document.querySelectorAll(".droppable");
+    draggableItems.forEach((elem) =>{
+        elem.addEventListener("dragstart", dragStart);
+    });
+    
+    droppableItems.forEach((elem) =>{
+        elem.addEventListener("dragover", dragOver);
+        elem.addEventListener("drop", drop);
+    });
+}
+
+const dragStart = (event) =>{
+    event.dataTransfer.setData("id", event.target.id);
+}
+
+const dragOver = (event) =>{
+    if (event.target.classList.contains("droppable")){
+        event.preventDefault();
+    }
+}
+
+const drop = (event) =>{
+    if (event.target.classList.contains("droppable")){
+        event.preventDefault();
+        const draggableElData = event.dataTransfer.getData("id");
+        const addedElement = document.getElementById(draggableElData);
+        event.target.append(addedElement);
+        addedElement.setAttribute("parent-id", event.target.getAttribute("drop-id"));
+        //event.target.append(document.getElementById(draggableElData));
+    }
+}
+
+const checkStorage = () =>{
+    if (localStorage.getItem("task-20")){
+        if (localStorage.getItem("task-20")[0]) spanResult.textContent = "Результат: " + localStorage.getItem("task-20")[0] + " из 10";
+        if (localStorage.getItem("task-20")[0] === "0") spanResult.textContent = "Результат: 0 из 10";  
+    }else 
+        spanResult.textContent = "Результат: - из 10";
+}
+
+//const cardData = randomize(data).slice(0, totalDraggableItems); //totalMatchingPairs
+matchingGenerator();
+let checkHint = false;
+checkStorage();
+commitMatching();
+
+btnCheck.addEventListener('click', ()=>{
+    count = 0, mark = 0;
+    //const elemsDraggable = sectionMatching.querySelectorAll("div.draggable");
+    const elemsDraggable = sectionMatching.querySelectorAll(".draggable");
+    const arrUserAnswers = [];
+    elemsDraggable.forEach((item)=>{
+        item.getAttribute("parent-id") === item.getAttribute("id")? count++ : count--;
+        arrUserAnswers.push({parentId: item.getAttribute("parent-id"), elemId: item.getAttribute("id")});
+    });
+    mark = count<=0? 0 : Math.round(count * score / totalDraggableItems);
+    console.log(arrUserAnswers, count, mark);
+    //save data web page
+    cond.sectionDraggable = sectionDraggable.querySelectorAll(".draggable");
+    cond.sectionMatching = sectionMatching.querySelectorAll(".draggable");
+    spanResult.textContent = "Результат: " + mark + " из " + score;
+    localStorage.setItem("task-20", [mark, checkHint]);
+});
+
+btnRestart.addEventListener('click', ()=>{
+    count = 0;
+    spanResult.textContent = "";
+    const elemsDraggable = document.querySelectorAll(".draggable");
+    randomize(elemsDraggable).forEach((item)=>{
+        sectionDraggable.append(item);
+    });
+    localStorage.removeItem("task-20");
+})
+
+let hint = false;
+const lampHint = document.querySelector("img.teory-hint");
+lampHint.addEventListener("click", () => {
+    hint = !hint;
+    const p_task = document.querySelectorAll("span.label");
+    checkHint = true;
+    localStorage.setItem("task-20", [mark, checkHint]);
+    if (hint){
+        [...p_task].forEach((el, ind)=> el.setAttribute("data-tooltip", cond.hints));
+        lampHint.style.transform = "scale(0.88)";
+    }else{
+        [...p_task].forEach((el, ind)=>el.removeAttribute("data-tooltip"));
+        lampHint.style.transform = "scale(1)";
+    }
+});
+
+
+const imgHint = document.querySelector("img.sistem-hint");
+imgHint.addEventListener("click", () => {
+    introJs().setOptions({
+    steps: [
+        
+        {
+            element: document.querySelector("p"),
+            intro: "Прочитайте условие"
+        },
+        {
+            element: document.querySelector(".draggable-items"),
+            intro: "Перетаскивайте текст из данного поля в поля, расположенные ниже"
+        }   
+    ]
+    }).start();
+});
+
---
+++ lesson-6-vector-projection/task-6-2-find-a-shadow/script_6_2.js
@@ -0,0 +1,259 @@
+// const fs = require('fs');
+// const dataJSON = fs.readFileSync(`${__dirname}/data_6_2.json`, 'utf-8');
+// const data = JSON.parse(dataJSON);
+// console.log('json', data);
+let svg = document.getElementById('pic'); 
+const form = document.querySelector('.form');
+const btnCheck = document.querySelector(".check");
+const btnBack = document.querySelector(".back");
+const btnReset = document.querySelector(".reset");
+const spanResult = document.querySelector(".span-result");
+
+let sizeSvg = 14, 
+    count = 0,
+    mark = 0;
+const variant = 0, 
+      coordAxes = [[1, sizeSvg+1, 1, 1], [0, sizeSvg,  sizeSvg+1, sizeSvg]];
+let rest = data[variant].vectors.length;
+let answInputs = [], 
+    arrInputs = [], 
+    colorProj = Array(data[variant].projections.length).fill('#dcdcdc'),
+    answColorVect = [];
+
+const draw_svg_line = (x1, y1, x2, y2) =>{
+    x1 *= 10; y1 *= 10; x2 *= 10; y2 *= 10;
+    const path_line = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    var s_line = `M ${x1} ${y1} L ${x2} ${y2} `;
+    path_line.setAttribute('d', s_line);
+    path_line.setAttribute('stroke-linecap', 'round');
+    path_line.setAttribute('stroke-linejoin', 'round');
+    path_line.setAttribute('stroke-width', '1');
+    path_line.setAttribute('stroke', '#E0E0E0');
+    
+    svg.appendChild(path_line);
+}
+
+const draw_svg_vect = (arr, color = '#414141', width, name) =>{
+    let x1 = arr[0], y1 = arr[1], x2 = arr[2], y2 = arr[3];
+    x1 *= 10; y1 *= 10; x2 *= 10; y2 *= 10;
+    const path_vect = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    
+    var dx = x2 - x1, dy = y2 - y1;
+    var len = Math.sqrt(dx * dx + dy * dy);
+    dx /= len; dy /= len;
+    var alen = 2.5;
+    var ax = x2 - (alen+1) * dx, ay = y2 - (alen) * dy;
+    
+    var s_vect = `M ${x1} ${y1} L ${x2} ${y2} ` +
+        " L " + (ax + dy * alen) + " " + (ay - dx * alen) +
+        " M " + x2 + " " + y2 +
+        " L " + (ax - dy * alen) + " " + (ay + dx * alen);
+    path_vect.setAttribute('d', s_vect);
+    path_vect.setAttribute('stroke-linecap', 'round');
+    path_vect.setAttribute('stroke-linejoin', 'round');
+    path_vect.setAttribute('stroke-width', width);
+    path_vect.setAttribute('stroke', color);
+    path_vect.setAttribute('data-len', len / 10);  
+    path_vect.classList.add(name);
+    svg.appendChild(path_vect);
+}
+
+const draw_svg_grid = () =>{
+    for (var i = 0; i < sizeSvg+2; ++i){
+        draw_svg_line(i, 0, i, sizeSvg+1);
+        draw_svg_line(0, i, sizeSvg+1, i);
+    }
+    for (let i = 0; i < coordAxes.length; i++) draw_svg_vect(coordAxes[i], '1.5', 'coordAxes');
+}
+
+const randomInt = (min, max) => {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
+//randomize
+const randomize = (arr) =>{
+    return [...arr].map(a => ({ value: a, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(a => a.value);   
+};
+
+data[variant].colorVect = randomize(data[variant].colorVect);
+data[variant].colorProj = colorProj;
+
+const drawTask = (colorVect, colorProj)=>{
+    const elH2 = document.querySelector('.condition');
+    elH2.innerHTML = `<div class="hint"><h3>${data[variant].condition[0]}</h3><img class="sistem-hint" src="./../../paint/hint.png"/><img class="teory-hint" src="./../../paint/lamp.png"/></div><p>${data[variant].condition[1]}</p>`;
+    draw_svg_grid();
+    //colorVectors = randomize(data[variant].color);
+    data[variant].vectors.forEach((el, index) => {
+        draw_svg_vect(el, colorVect[index], '2', 'vector'); 
+    });
+    data[variant].projections.forEach((el, ind)=>{
+        draw_svg_vect(el, colorProj[ind], '2', 'projection');
+    });
+    console.log(data);
+}
+
+const clickVectors = (e, skalar)=>{
+    const clickedVect = e.target;
+    if (!clickedVect.classList.contains("clicked")){
+        clickedVect.classList.add("clicked");
+        clickedVect.setAttribute('stroke-width', '3');
+        const chooseVectors = document.querySelectorAll(".clicked");
+        let color;
+        if (chooseVectors.length === 2){
+            if (chooseVectors[0].classList.contains('projection'))
+                color = chooseVectors[0].getAttribute('stroke') === '#dcdcdc'? chooseVectors[1].getAttribute('stroke'): chooseVectors[0].getAttribute('stroke');
+            else 
+                color = chooseVectors[1].getAttribute('stroke') === '#dcdcdc'? chooseVectors[0].getAttribute('stroke'): chooseVectors[1].getAttribute('stroke');
+
+            chooseVectors.forEach((elem)=>{
+                elem.setAttribute('stroke', color);
+                elem.classList.remove("clicked");
+                if (elem.classList.contains('vector')) elem.style.pointerEvents = "none";
+                setTimeout(()=>{ 
+                   elem.setAttribute('stroke-width', '1.7');
+                }, 700);
+
+            form.innerHTML = `<p>Скалярная проекция выбранного вектора равна:</p><div class="task"><div class="color" style="background-color: ${color}"></div><input skalar = "${skalar}" style='text'></div>`;
+
+            const input = document.querySelector('input');
+            input.addEventListener('change', ()=>{
+                answInputs.push(input.value);
+                form.innerHTML = '';
+                arrInputs.push(skalar);
+                console.log('input change', answInputs, arrInputs);
+
+                });
+            });
+        }
+    }else{
+        clickedVect.classList.remove("clicked");
+        clickedVect.setAttribute('stroke-width', '2');
+    }
+}
+
+const talkColor=(color)=>{
+    switch (color){
+        case '#F2E01F': return 'жёлтый';
+        case '#963E2A': return 'коричневый';
+        case '#F2E01F': return 'жёлтый';
+        case '#FF3B84': return 'розовый';
+        case '#38CEF3': return 'голубой';
+        case '#2788DB': return 'синий';
+        case '#2CF2AD': return 'зеленый';
+        case '#B87FB6': return 'фиолетовый';  
+        case '#FF6947': return 'оранжевый'; 
+        case '#dcdcdc': return 'серый'; 
+            
+    }
+}
+
+const addEventToVect = (arr1, arr2)=>{
+    arr1.forEach((el, index)=>{
+        el.classList.add(arr2[index][4]);
+        el.addEventListener('click', (e)=>{
+            clickVectors(e, arr2[index][5]);
+        });   
+    });   
+}
+
+const matchingGenerator = ()=>{
+    const arrVect = svg.querySelectorAll('path.vector');
+    const arrProj = svg.querySelectorAll('path.projection');
+    addEventToVect(arrVect, data[variant].vectors);
+    addEventToVect(arrProj, data[variant].projections);
+}
+
+const checkStorage = () =>{
+    if (localStorage.getItem("task-21")){
+        if (localStorage.getItem("task-21")[0]) spanResult.textContent = "Результат: " + localStorage.getItem("task-21")[0] + " из 10";
+        if (localStorage.getItem("task-21")[0] === "0") spanResult.textContent = "Результат: 0 из 10";  
+    }else 
+        spanResult.textContent = "Результат: - из 10";
+}
+
+drawTask(data[variant].colorVect, data[variant].colorProj);
+let checkHint = false;
+checkStorage();
+matchingGenerator();
+
+btnCheck.addEventListener('click', ()=>{
+    for (let i=0; i<data[variant].projections.length; i++){
+        const sameElem = svg.querySelectorAll(`.group-${i+1}`);
+        const colorElem = sameElem[0].getAttribute('stroke');
+        if ([...sameElem].every(el=>el.getAttribute('stroke') === colorElem)) count++; else count--;  
+    }
+    arrInputs.forEach((el, ind)=>{
+        if (el == answInputs[ind]) count++; else count--;
+    });
+    mark = count <= 0 ? 0 : Math.round(count * data[variant].score / data[variant].vectors.length * 2);
+    data[variant].rightAnswerInput = arrInputs;
+    data[variant].userAnswerInput = answInputs;
+    data[variant].mark = mark;
+    arrInputs = [], answInputs = [], answColorVect = [], count = 0, mark = 0;
+    console.log("check", data);
+    //save data web page
+    const vect = svg.querySelectorAll('.vector');
+    const colorVect =[...vect].map(el=> el.getAttribute('stroke'));
+    const proj = svg.querySelectorAll('.projection');
+    const colorProj =[...proj].map(el=> el.getAttribute('stroke'));
+    data[variant].colorProj = colorProj;
+    data[variant].colorVect = colorVect;
+    spanResult.textContent = "Результат: " + mark + " из " + data[variant].score;
+    localStorage.setItem("task-21", [mark, checkHint]);
+});
+
+btnReset.addEventListener('click', ()=>{
+    arrInputs = [], answInputs = [], answColorVect = [], count = 0, mark = 0;
+    const arrVect = svg.querySelectorAll('path.vector');
+    const arrProj = svg.querySelectorAll('path.projection');
+    arrVect.forEach((el, index)=>{
+        el.setAttribute('stroke', data[variant].colorVect[index]);
+        el.style.pointerEvents = "all";
+    });
+    arrProj.forEach((el, index)=>{
+        el.setAttribute('stroke', '#dcdcdc');
+    });
+    form.replaceChildren();
+    data[variant].rightAnswerInput = arrInputs;
+    data[variant].userAnswerInput = answInputs;
+    data[variant].mark = mark;
+    colorProj = Array(data[variant].projections.length).fill('#dcdcdc');
+    data[variant].colorProj = colorProj;
+    console.log("reset", data);
+    localStorage.removeItem("task-21");
+});
+
+let hint = false;
+const lampHint = document.querySelector("img.teory-hint");
+lampHint.addEventListener("click", () => {
+    hint = !hint;
+    const p_task = document.querySelector("p");
+    checkHint = true;
+    localStorage.setItem("task-21", [mark, checkHint]);
+    if (hint){
+        p_task.setAttribute("data-tooltip", "Геометрическая проекция вектора — это вектор, который можно получить, если провести перпендикуляры от концов вектора до выбранной оси. Скалярной проекцией (Пр ) вектора на ось L называется скаляр, абсолютная величина которого равна модулю векторной проекции того же вектора на ту же ось.");
+        lampHint.style.transform = "scale(0.88)";
+    }else{
+        p_task.removeAttribute("data-tooltip");
+        lampHint.style.transform = "scale(1)";
+    }
+});
+
+
+const imgHint = document.querySelector("img.sistem-hint");
+imgHint.addEventListener("click", () => {
+    introJs().setOptions({
+    steps: [
+        
+        {
+            element: document.querySelector("p"),
+            intro: "Прочитайте условие"
+        },
+        {
+            element: document.querySelector("svg"),
+            intro: "Нажимайте на пары - цветной вектор и его тень"
+        }        
+    ]
+    }).start();
+});
